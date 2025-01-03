1a990fb34403e43e77e750f61db432ab32793ed6
Jasec <jasec_7@hotmail.com>
2025-01-03 03:09:56 -0800

    complete arithmetic lab

---
+++ index.js
@@ -0,0 +1,54 @@
+function add() {
+
+}
+
+function subtract() {
+
+}
+
+function multiply(){
+
+}
+
+function divide() {
+
+}
+
+function add(x , y) {
+    return x + y;
+}
+console.log(add(100 , 95));
+
+function subtract(x , y) {
+    return x - y;
+}
+console.log(subtract(100 , 491))
+
+function multiply( x , y) {
+    return x * y;
+}
+console.log(multiply(158796 , 2));
+
+function divide(x , y) {
+    return x / y;
+}
+
+function increment(number) {
+    return number += 1;
+}
+
+function decrement(number) {
+    return number -= 1;
+}
+ 
+function makeInt(n) {
+    return parseInt(n);
+}
+
+function makeInt(n) {
+    return parseInt(n , 10)
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
